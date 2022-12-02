55bbe6fc0619217380c58e27f28fe4b673880b1f
Al-ta-iR <artyom.oleynik@gmail.com>
2022-12-02 14:34:25 +0100

    add sleep 30 seconds and changes find-method

--- test_items.py
+++ test_items.py
@@ -1,12 +1,9 @@
-
+import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_find_add_to_card_button(browser):
+def test_find_add_to_cart_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    assert WebDriverWait(browser, 5).until(
-        EC.visibility_of_element_located((By.CLASS_NAME, "btn-add-to-basket"))
-    ).is_displayed(), \
+    time.sleep(30)
+    assert browser.find_element(By.CLASS_NAME, 'btn-add-to-basket').is_displayed(), \
         'Кнопка добавления товара в корзину отсутсвует'
